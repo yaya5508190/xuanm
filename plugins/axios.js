2ea2d0900fae5f3058ca32408ddd2bf6836af8b3
yingxiang <yingxiang@jiudaotech.com>
2018-03-22 00:15:59 +0800

    修复axios baseurl的问题 忽略上传文件夹

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 
+const host = process.env.HOST || '127.0.0.1'
+const port = process.env.PORT || 3000
 if (process.browser) {
   axios.defaults.baseURL = '/api'
 } else {
-  axios.defaults.baseURL = 'http://localhost:3000/api'
+  axios.defaults.baseURL = 'http://' + host + ':' + port + '/api'
 }
 
 export default axios
